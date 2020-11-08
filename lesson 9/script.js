bb60b28b2926151445350b6cf56d336396773d6c
robocodebr <robocodebr@gmail.com>
2020-11-08 20:03:03 +0200

    дуыыщт10

---
+++ lesson 9/script.js
@@ -0,0 +1,42 @@
+// console.log("jdjdjdjjd");
+
+//      let j = 0; 
+//       while(j>=10) {
+//           console.log("j")
+//                     i--;
+//          }
+
+//          console.log("j= "+j);
+//          let sum = 0;
+//          for(j = 0 ; t<10 ; t+=5){
+//  console.log(t)
+//  sum += t; }
+//  console.log("sum="+sum);
+
+//  let average = 0;
+//  for(let t = 0 ; t<=20 ; t+=1);U
+//      average += );U
+//  }
+//  average = average/21;
+//  console.log("average ="+average);
+//  let min = 100;
+//  let i = 0;
+//  while(i<10)
+//  let r = Math.coil(Math.random()*100);
+//  if(min>r){
+//  min=r;
+// }
+// console.log(min);
+// let r = 0;
+// do {
+//     console.log(r);
+//     r++;
+
+// }while(r<10);
+let r = 45;
+do {
+    console.log(r);
+    r++;
+
+}while(r<650);
+        
